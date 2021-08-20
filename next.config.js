722fa15521ab967c268c32e9a7ab75e4196c94ee
saurabh-dev15 <saurabh.singhdev15@gmail.com>
2021-08-21 00:41:42 +0530

    env

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
 const withImages = require('next-images');
-module.exports = withImages();
+// module.exports = withImages();
+
+module.exports = {
+    ...withImages(),
+    reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_APP_ENVIRONMENT: process.env.NEXT_PUBLIC_APP_ENVIRONMENT,
+    }
+  }
