6972f8d49ae72be7b62eda4ec11faa978b16920f
Saurabh Singh <saurabh.singhdev15@gmail.com>
2023-08-04 12:54:44 +0530

    apply bug

--- api/saveOrderData.js
+++ api/saveOrderData.js
@@ -1,5 +1,14 @@
+let userCompany
+if (typeof window !== 'undefined') {
+    userCompany = localStorage.getItem('companyName')
+}
+
+
+const dynamicUrl= urlObjects[userCompany]
+
+
 export const saveOrderData=async (data)=>{
-    let url = `https://api.airtable.com/v0/appoZqa8oxVNB0DVZ/Orders`;
+    let url = `https://api.airtable.com/v0/${dynamicUrl?.key}/Orders`;
    
     const res =  await fetch(url, {
       method: "post",
