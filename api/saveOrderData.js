e83f8a2039d5a7e7d09ec95d9dc9d81b52442019
Saurabh Singh <saurabh.singhdev15@gmail.com>
2023-08-10 09:27:12 +0530

    BUGS

--- api/saveOrderData.js
+++ api/saveOrderData.js
@@ -1,28 +1,30 @@
 import { urlObjects } from "../UTILS/urlObjects"
 
-let userCompany
-if (typeof window !== 'undefined') {
+
+
+
+export const saveOrderData = async (data) => {
+
+  let userCompany
+  if (typeof window !== 'undefined') {
     userCompany = localStorage.getItem('companyName')
-}
-
-
-const dynamicUrl= urlObjects[userCompany]
-
-
-export const saveOrderData=async (data)=>{
-    let url = `https://api.airtable.com/v0/${dynamicUrl?.key}/Orders`;
-
-    
-   
-    const res =  await fetch(url, {
-      method: "post",
-      headers: new Headers({
-        Authorization: "Bearer key0AV84zSplHpV5B",
-        'Content-Type': 'application/json'
-      }),
-      body:JSON.stringify(data)
-    });
-    const responseData=await res.json()
-    return responseData
-     
   }
+
+
+  const dynamicUrl = urlObjects[userCompany]
+  let url = `https://api.airtable.com/v0/${dynamicUrl?.key}/Orders`;
+
+
+
+  const res = await fetch(url, {
+    method: "post",
+    headers: new Headers({
+      Authorization: "Bearer key0AV84zSplHpV5B",
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify(data)
+  });
+  const responseData = await res.json()
+  return responseData
+
+}
