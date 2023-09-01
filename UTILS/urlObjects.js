0078e4657480f34bff4ed6ede2919cf25bca402a
Saurabh Singh <saurabh.singhdev15@gmail.com>
2023-09-02 00:32:01 +0530

    bug latest

--- UTILS/urlObjects.js
+++ UTILS/urlObjects.js
@@ -1,6 +1,6 @@
  export const urlObjects = {
     "gscourtyardhomes": {
-        clientProfile:"https://api.airtable.com/v0/appNSZE4sLntsJdpb/Client%20List%20%26%20Profiles?maxRecords=100&view=Client%20List%20%26%20General%20Info",
+        clientProfile:"https://api.airtable.com/v0/appoZqa8oxVNB0DVZ/Client%20Profile",
         getMarkup:"https://api.airtable.com/v0/appoZqa8oxVNB0DVZ/Client%20Profile/recgPVsLYxndCLFLP",
         getFloorPlan:"https://api.airtable.com/v0/appoZqa8oxVNB0DVZ/Floorplan%20Costs?maxRecords=100&view=Grid%20view",
         getConstructionCost:"https://api.airtable.com/v0/appoZqa8oxVNB0DVZ/Construction%20Options?maxRecords=100&view=Grid%20view",
@@ -17,12 +17,12 @@
 
         However, please note that the pricing does not include: Steps, driveway, septic, Well, seed and straw, landscaping, & all other unforseen site conditions (ex. Limestone under your ground), etc
         
-           Your selected options are below. Note, you are able to make additional changes when representative reaches out, or you can feel free to call us in advance at 812-322-5151`,
+           Your selected options are below. Note, you are able to make additional changes when representative reaches out, or you can feel free to call us in advance at (812) 675-8541`,
         
 
     },
     "fawaffordablehomes":{
-        clientProfile:"https://api.airtable.com/v0/appNSZE4sLntsJdpb/Client%20List%20%26%20Profiles?maxRecords=100&view=Client%20List%20%26%20General%20Info",
+        clientProfile:"https://api.airtable.com/v0/appqwrDbUmW3KAXGN/Client%20Profile",
         getMarkup:"https://api.airtable.com/v0/appqwrDbUmW3KAXGN/Client%20Profile/recZ7XckhoxP71D9X",
         getFloorPlan:"https://api.airtable.com/v0/appqwrDbUmW3KAXGN/Floorplan%20Costs?maxRecords=100&view=Grid%20view",
         getConstructionCost:"https://api.airtable.com/v0/appqwrDbUmW3KAXGN/Construction%20Options?maxRecords=100&view=Grid%20view",
@@ -34,11 +34,11 @@
         key:"appqwrDbUmW3KAXGN",
         email:"hjanssen79@gmail.com",
         Title:"Faw Afforable Homes",
-        templateDescription:(totalPrice)=>`Thes estimate of your dream home is $${totalPrice}, which includes the base price ,site prep, and all your customizations. Additionally, all pricing includes: Foundation, backfill, delivery, set-up, all interior and exterior finish work, HVAC (excluding duct work beneath modular homes), gutters, cleaning, sales tax and one of steps.
+        templateDescription:(totalPrice)=>`The estimate of your dream home is $${totalPrice}, which includes the base price, site prep, and all your customizations. Additionally, all pricing includes: Foundation, backfill, delivery, set-up, all interior and exterior finish work, HVAC (excluding duct work beneath modular homes), gutters, cleaning, sales tax and one of steps.
 
         However, please note that the pricing does not include: Utility hooks-up, driveway, septic, well, seed and straw, landscaping, & resolving all other unforeseen site conditions.
         
-             Your selected options are listed below. Prior to ordering the home, you can make additional changes to your home design by calling your representative at 217-508-7920`,
+             Your selected options are listed below. Prior to ordering the home, you can make additional changes to your home design by calling your representative at (217) 508-8806`,
         
 
     }
